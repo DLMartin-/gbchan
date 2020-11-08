52a0ae737b057a3dffead123e2fff4d0dff42cfb
Danny Martin <ashinyacorn@gmail.com>
2020-11-08 04:20:25 -0500

    Implemented the fetch-decode-execute cycle for cpu

--- libs/cpu/inc/cpu/cpu.h
+++ libs/cpu/inc/cpu/cpu.h
@@ -1,17 +1,28 @@
 #ifndef CPU_INC_CPU_CPU_H_
 #define CPU_INC_CPU_CPU_H_
 #include <cstdint>
+#include <cstddef>
 
 namespace gb {
   class cpu {
     enum struct stack_pointer : std::uint16_t {};
     enum struct program_counter : std::uint16_t {};
     enum struct opcode : std::uint8_t {};
+    static constexpr opcode CbPrefix {0xcb};
+    using instruction = std::uint16_t(*)(cpu*) noexcept;
 
     public:
-      std::uint16_t run_cycle() noexcept;
+      cpu(std::byte* mmu) noexcept;
 
+      std::uint16_t run_cycle() noexcept;
     private:
+      std::byte read_mmu() noexcept;
+      opcode fetch() noexcept;
+      instruction decode(opcode opcode) noexcept;
+      instruction decode_cb(opcode opcode) noexcept;
+      std::uint16_t execute(instruction instruction) noexcept;
+      
+      std::byte* mmu_ {nullptr};
       stack_pointer sp_ {0};
       program_counter pc_ {0};
       std::uint16_t cycles_run_ {0};
--- libs/cpu/src/cpu.cpp
+++ libs/cpu/src/cpu.cpp
@@ -1,8 +1,56 @@
 #include "cpu.h"
 
+#include <functional>
+
 namespace gb {
 
   std::uint16_t cpu::run_cycle() noexcept {
-    return 0;
+    auto opcode = fetch();
+    instruction instruction {nullptr};
+    if(opcode == cpu::CbPrefix) {
+      opcode = fetch();
+      instruction = decode_cb(opcode);
+    }
+    else {
+      instruction = decode(opcode);
+    }
+
+    return execute(instruction);
+  }
+
+  cpu::opcode cpu::fetch() noexcept {
+    return static_cast<cpu::opcode>(read_mmu());
+  }
+
+  std::byte cpu::read_mmu() noexcept {
+    auto idx = static_cast<std::uint16_t>(pc_);
+    pc_ = static_cast<cpu::program_counter>(static_cast<std::uint16_t>(pc_) + 1);
+    return mmu_[idx];
+  }
+
+  cpu::instruction cpu::decode(cpu::opcode opcode) noexcept {
+    cpu::instruction instruction_set[] = {
+      [](cpu* cpu) noexcept -> std::uint16_t { return 0; },
+      [](cpu* cpu) noexcept -> std::uint16_t { return 1; },
+      //To-do: Flesh out remaining 256 instructions
+    };
+
+    auto const idx = static_cast<uint8_t>(opcode);
+    return instruction_set[idx];
+  }
+
+  cpu::instruction cpu::decode_cb(cpu::opcode opcode) noexcept {
+    cpu::instruction instruction_set[] = {
+      [](cpu* cpu) noexcept -> std::uint16_t { return 0; },
+      [](cpu* cpu) noexcept -> std::uint16_t { return 1; },
+      //To-do: Flesh out remaining 256 cb-prefixed instructions
+    };
+
+    auto const idx = static_cast<uint8_t>(opcode);
+    return instruction_set[idx];
+  }
+
+  std::uint16_t cpu::execute(cpu::instruction instruction) noexcept {
+    return std::invoke(instruction, this);
   }
 }
